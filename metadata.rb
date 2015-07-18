bae8e2a55d2aa0a319c805f0290144692432293a
Colin Shea <colin@evaryont.me>
2015-07-18 21:31:24 +0000

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,10 @@ maintainer_email 'maciej@3ofcoins.net'
 license 'MIT'
 description 'Installs and configures Tinc VPN'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
 
 supports 'ubuntu', '>= 12.04'
+supports 'arch'
+supports 'debian', '>= 7'
 
 depends 'sysctl'
