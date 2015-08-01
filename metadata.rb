f05cd8c63f076b4eeccfbc748f8e773578cf9ac1
Colin Shea <colin@evaryont.me>
2015-08-01 03:27:58 +0000

    Revert "version bump"
    This reverts commit bae8e2a55d2aa0a319c805f0290144692432293a.

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,8 @@ maintainer_email 'maciej@3ofcoins.net'
 license 'MIT'
 description 'Installs and configures Tinc VPN'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.1'
+version '0.1.0'
 
 supports 'ubuntu', '>= 12.04'
-supports 'arch'
-supports 'debian', '>= 7'
 
 depends 'sysctl'
